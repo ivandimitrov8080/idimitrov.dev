994cbc20e886b3e6e13fcf712e7b7d609e853336
Ivan Dimitrov <ivan@idimitrov.dev>
2023-11-07 17:27:31 +0200

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+import GithubSvg from "@/components/svg/github-svg";
+import GitlabSvg from "@/components/svg/gitlab-svg";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="place-content-center">
-      <div>
-        whaaa
+    <main className="place-content-center text-center">
+      <div className="flex flex-row space-x-4">
+        <Link href={process.env.NEXT_PUBLIC_GITHUB_URL!} target="_blank">
+          <GithubSvg />
+        </Link>
+        <Link href={process.env.NEXT_PUBLIC_GITLAB_URL!} target="_blank">
+          <GitlabSvg />
+        </Link>
       </div>
     </main>
   )
