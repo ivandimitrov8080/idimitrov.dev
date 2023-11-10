94e40ec8ede59c5715903b5b6cda87c496b48c2b
Ivan Dimitrov <ivan@idimitrov.dev>
2023-11-10 18:28:45 +0200

    keys for array

--- src/app/components/links.tsx
+++ src/app/components/links.tsx
@@ -5,11 +5,11 @@ import Link from "next/link";
 export default function Links() {
 
   const links = [
-    <Link aria-label="GitHub" href={process.env.NEXT_PUBLIC_GITHUB_URL!} target="_blank">
+    <Link key="github" aria-label="GitHub" href={process.env.NEXT_PUBLIC_GITHUB_URL!} target="_blank">
       <FontAwesomeIcon icon={faGithub} />
     </Link>
     ,
-    <Link aria-label="GitLab" href={process.env.NEXT_PUBLIC_GITLAB_URL!} target="_blank">
+    <Link key="gitlab" aria-label="GitLab" href={process.env.NEXT_PUBLIC_GITLAB_URL!} target="_blank">
       <FontAwesomeIcon icon={faGitlab} />
     </Link>
   ]
