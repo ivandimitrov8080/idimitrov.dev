23f98d414ced6beba7424be2a6d2d6d327a81b7b
Ivan Dimitrov <ivan@idimitrov.dev>
2023-11-10 18:27:23 +0200

    using fa icons

--- src/app/components/links.tsx
+++ src/app/components/links.tsx
@@ -1,24 +1,22 @@
-import GithubSvg from "@/components/svg/github-svg";
-import GitlabSvg from "@/components/svg/gitlab-svg";
+import { faGithub, faGitlab } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
 export default function Links() {
 
   const links = [
     <Link aria-label="GitHub" href={process.env.NEXT_PUBLIC_GITHUB_URL!} target="_blank">
-      <GithubSvg />
+      <FontAwesomeIcon icon={faGithub} />
     </Link>
     ,
     <Link aria-label="GitLab" href={process.env.NEXT_PUBLIC_GITLAB_URL!} target="_blank">
-      <GitlabSvg />
+      <FontAwesomeIcon icon={faGitlab} />
     </Link>
   ]
 
   return (
-    <main className="place-content-center text-center">
-      <div className={`grid grid-cols-${links.length} gap-4`}>
-        {links}
-      </div>
-    </main>
+    <div className={`grid grid-cols-${links.length} gap-4`}>
+      {links}
+    </div>
   )
 }
