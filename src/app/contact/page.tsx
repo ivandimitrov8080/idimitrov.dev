905d7edaccf2c2fd447f5cae35ef7bd88d86a2b1
Ivan Dimitrov <ivan@idimitrov.dev>
2023-11-11 14:46:35 +0200

    contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,14 @@
-export default function Navbar() {
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function Contact() {
+
+  const email = "ivan@idimitrov.dev";
 
   return (
-    <div className="w-full h-max p-2 grid place-content-center">
-      <div className="flex flex-row gap-6">
+    <div className="w-full h-full p-2 grid place-content-center">
+      <div className="flex flex-row gap-4">
+        <a href={`mailto:${email}`}><FontAwesomeIcon icon={faEnvelope}/></a>
       </div>
     </div>
   )
