c36d0308d2a9766174fde8432866e572a4b82d5e
Ivan Dimitrov <ivan@idimitrov.dev>
2023-11-11 14:50:00 +0200

    cases list styles

--- src/app/components/cases.tsx
+++ src/app/components/cases.tsx
@@ -5,7 +5,7 @@ import { getCases } from "../lib/content";
 export default function Cases() {
   const cases: GrayMatterFile<string>[] = getCases()
   return (
-    <div className="p-20">
+    <div className="p-20 w-3/4 mx-auto">
       {cases.map((c) => {
         const d = c.data;
         const date = d.date.split("-")
@@ -13,7 +13,7 @@ export default function Cases() {
         const to = date[1]?.trim()
         return (
           <div key={d.slug} className="w-full h-max flex justify-center">
-            <Link className="btn flex flex-col w-max text-center" href={d.slug}>
+            <Link className="btn flex flex-col w-full text-center" href={d.slug}>
               <span className="text-lg px-6">{d.title}</span>
               {from} {to ? `- ${to}` : ""}
             </Link>
