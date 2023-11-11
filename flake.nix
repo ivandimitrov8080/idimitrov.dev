6fa36883854c2e702a0c8b9fa6c9e397e4981575
Ivan Dimitrov <ivan@idimitrov.dev>
2023-11-11 11:51:15 +0200

    style fixes

--- flake.nix
+++ flake.nix
@@ -20,7 +20,9 @@
         tmux new-session -s my_session -d
         tmux new-window -t my_session:1
         tmux new-window -t my_session:2
+        tmux new-window -t my_session:3
         tmux send-keys -t my_session:1.0 'vi' C-m
+        tmux send-keys -t my_session:3.0 'bun run dev' C-m
         tmux attach-session -t my_session
       '';
     in
--- src/app/components/links.tsx
+++ src/app/components/links.tsx
@@ -5,13 +5,15 @@ import Link from "next/link";
 export default function Links() {
 
   return (
-    <div className={`grid grid-cols-2 gap-4`}>
-      <Link aria-label="GitHub" href={process.env.NEXT_PUBLIC_GITHUB_URL!} target="_blank">
-        <FontAwesomeIcon icon={faGithub} />
-      </Link>
-      <Link aria-label="GitLab" href={process.env.NEXT_PUBLIC_GITLAB_URL!} target="_blank">
-        <FontAwesomeIcon icon={faGitlab} />
-      </Link>
+    <div className="grid w-full h-full place-content-center">
+      <div className={"grid grid-cols-2 gap-4 place-content-center"}>
+        <Link aria-label="GitHub" href={process.env.NEXT_PUBLIC_GITHUB_URL!} target="_blank">
+          <FontAwesomeIcon icon={faGithub} />
+        </Link>
+        <Link aria-label="GitLab" href={process.env.NEXT_PUBLIC_GITLAB_URL!} target="_blank">
+          <FontAwesomeIcon icon={faGitlab} />
+        </Link>
+      </div>
     </div>
   )
 }
