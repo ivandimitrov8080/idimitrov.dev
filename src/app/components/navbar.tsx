40c1d0b0a260746873ebaa3fb73759164b5dc30f
Ivan Dimitrov <ivan@idimitrov.dev>
2023-11-11 19:58:35 +0200

    fixing build error - not everything can be nice

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,21 +2,25 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-const link = (text: string, href: string) => {
+const Navbar = () => {
   const path = usePathname()
+  const link = (text: string, href: string) => {
+    return (
+      <Link data-selected={path === href} className="btn" aria-label={text} href={href}>
+        {text}
+      </Link>
+    )
+  }
   return (
-    <Link data-selected={path === href} className="btn" aria-label={text} href={href}>
-      {text}
-    </Link>
+    <div className="w-max h-max px-6 py-2 mx-auto rounded-full bg-slate-900 grid place-content-center">
+      <div className="flex flex-row gap-6">
+        {link("Home", "/")}
+        {link("Cases", "/cases")}
+        {link("Contact", "/contact")}
+      </div>
+    </div>
   )
 }
-const Navbar = () =>
-  <div className="w-max h-max px-6 py-2 mx-auto rounded-full bg-slate-900 grid place-content-center">
-    <div className="flex flex-row gap-6">
-      {link("Home", "/")}
-      {link("Cases", "/cases")}
-      {link("Contact", "/contact")}
-    </div>
-  </div>
+
 
 export default Navbar
