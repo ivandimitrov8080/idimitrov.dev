d09bd61eba53c403522d046a48ee6b66f62f9ffb
Ivan Dimitrov <ivan@idimitrov.dev>
2023-11-11 14:02:47 +0200

    more refactor and contact page

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,15 +4,21 @@ import { usePathname } from "next/navigation";
 
 export default function Navbar() {
   const path = usePathname()
+
+  const link = (text: string, href: string) => {
+    return (
+      <Link aria-selected={path === href} className="btn" aria-label="Home" href={href}>
+        {text}
+      </Link>
+    )
+  }
+
   return (
     <div className="w-full h-max p-2 grid place-content-center">
       <div className="flex flex-row gap-6">
-        <Link aria-selected={path === "/"} className="btn" aria-label="Home" href="/">
-          Home
-        </Link>
-        <Link aria-selected={path === "/cases"} className="btn" aria-label="Cases" href="/cases">
-          Cases
-        </Link>
+        {link("Home", "/")}
+        {link("Cases", "/cases")}
+        {link("Contact", "/contact")}
       </div>
     </div>
   )
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+
+  return (
+    <div className="w-full h-max p-2 grid place-content-center">
+      <div className="flex flex-row gap-6">
+      </div>
+    </div>
+  )
+}
