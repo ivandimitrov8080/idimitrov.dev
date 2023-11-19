df74cb16c88bcb05181945d9684aee308013da55
Ivan Dimitrov <ivan@idimitrov.dev>
2023-11-19 08:13:06 +0200

    refactor some pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
-import Links from "$components/links";
+import { faGithub, faGitlab } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
-const Home = () => <Links />
+const Home = () =>
+  <div className="grid w-full h-full place-content-center">
+    <div className={"grid grid-cols-2 gap-4 place-content-center"}>
+      <Link aria-label="GitHub" href={process.env.NEXT_PUBLIC_GITHUB_URL || ""} target="_blank">
+        <FontAwesomeIcon className="w-14 h-14" icon={faGithub} />
+      </Link>
+      <Link aria-label="GitLab" href={process.env.NEXT_PUBLIC_GITLAB_URL || ""} target="_blank">
+        <FontAwesomeIcon className="w-14 h-14" icon={faGitlab} />
+      </Link>
+    </div>
+  </div>
 
 export default Home;
