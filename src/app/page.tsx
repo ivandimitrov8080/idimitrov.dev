abb9531c95fd97441082ca956ee00884a27d21be
Ivan Dimitrov <ivan@idimitrov.dev>
2023-12-28 10:34:42 +0200

    change gitea url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 const Home = () => (
   <div className="grid w-full h-full place-content-center">
     <div className={"grid grid-cols-3 gap-4 place-content-center"}>
-      <Link aria-label="GitLab" href={process.env.NEXT_PUBLIC_GITEA_URL || "https://git.idimitrov.dev/ivan"} target="_blank">
+      <Link aria-label="GitLab" href={process.env.NEXT_PUBLIC_GITEA_URL || "https://src.idimitrov.dev/ivan"} target="_blank">
         <GiteaSvg />
       </Link>
       <Link aria-label="GitHub" href={process.env.NEXT_PUBLIC_GITHUB_URL || "https://github.com/ivandimitrov8080"} target="_blank">
