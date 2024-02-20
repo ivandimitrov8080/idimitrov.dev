e158d735ce1a22e015b1cf196b303174c254f5c4
Ivan Dimitrov <ivan@idimitrov.dev>
2024-02-20 18:39:55 +0200

    add a links md page

---
+++ _content/links.md
@@ -0,0 +1,12 @@
+---
+title: Links
+goal: Links to notable things
+role: 
+date: 
+z: 9
+draft: false
+---
+
+
+* [My firm registration (IDimitrov Ltd.)](https://portal.registryagency.bg/CR/en/Reports/ActiveConditionTabResult?uic=207693833)
+
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import "./globals.css";
 import Navbar from "$components/navbar";
 import type { Metadata } from "next";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCakeCandles, faSquareFull } from "@fortawesome/free-solid-svg-icons";
 
 export const metadata: Metadata = {
   title: "Ivan Dimitrov",
@@ -10,7 +8,6 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  let today = new Date();
   return (
     <html lang="en">
       <body>
