e519cafb59a1c6b4b14d88ce70ff02a84b89517d
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2024-09-13 22:36:32 +0200

    links border

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -4,17 +4,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
 const Links = () => (
-  <div className="grid w-max h-max grid-cols-2 grid-rows-2 gap-12 place-content-center absolute right-96 top-[38%]">
-    <Link aria-label="GitHub" href={process.env.NEXT_PUBLIC_GITHUB_URL || "https://github.com/ivandimitrov8080"} target="_blank">
+  <div className="grid w-max h-max grid-cols-2 grid-rows-2 gap-0 place-content-center absolute right-96 top-[35%] circle-gradient">
+    <Link className="p-5 mr-[1px] bg-slate-950" aria-label="GitHub" href={process.env.NEXT_PUBLIC_GITHUB_URL || "https://github.com/ivandimitrov8080"} target="_blank">
       <FontAwesomeIcon className="w-14 h-14" icon={faGithub} />
     </Link>
-    <Link aria-label="GitLab" href={process.env.NEXT_PUBLIC_GITEA_URL || "https://src.idimitrov.dev/ivandimitrov8080"} target="_blank">
+    <Link className="p-5 bg-slate-950" aria-label="GitLab" href={process.env.NEXT_PUBLIC_GITEA_URL || "https://src.idimitrov.dev/ivandimitrov8080"} target="_blank">
       <GiteaSvg />
     </Link>
-    <Link aria-label="LinkedIn" href={process.env.NEXT_PUBLIC_LINKEDIN_URL || "https://www.linkedin.com/in/devidimitrov"} target="_blank">
+    <Link className="p-5 mr-[1px] mt-[1px] bg-slate-950" aria-label="LinkedIn" href={process.env.NEXT_PUBLIC_LINKEDIN_URL || "https://www.linkedin.com/in/devidimitrov"} target="_blank">
       <FontAwesomeIcon className="w-14 h-14" icon={faLinkedin} />
     </Link>
-    <Link aria-label="Upwork" href={process.env.NEXT_PUBLIC_UPWORK_URL || "https://www.upwork.com/freelancers/idimitrov"} target="_blank">
+    <Link className="p-5 mt-[1px] bg-slate-950" aria-label="Upwork" href={process.env.NEXT_PUBLIC_UPWORK_URL || "https://www.upwork.com/freelancers/idimitrov"} target="_blank">
       <FontAwesomeIcon className="w-14 h-14" icon={faUpwork} />
     </Link>
   </div>
