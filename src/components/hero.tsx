1349dd0fff4cf2822d8a59599e0fbf98cfe81c5e
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2024-09-13 23:34:02 +0200

    hero text redo

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,7 +4,8 @@ const Hero = () => (
   <div className="grid px-14 py-48 gap-12">
     <div className="w-1/2">
       <p className="text-3xl text-[#FB923C]">Software Developer</p>
-      <p className="text-8xl text-white">Optimal solution for a specific problem</p>
+      <p className="text-8xl text-white capitalize">Full stack web development</p>
+      <p className="text-8xl capitalize text-transparent font-bold bg-gradient-to-br from-transparent via-red-400 to-yellow-400 bg-clip-text">optimized</p>
     </div>
     <Links />
   </div>
