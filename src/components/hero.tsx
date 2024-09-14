f7037a608e680c231626c80ea8485625fab536c6
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2024-09-14 20:51:20 +0200

    cases button

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,11 +1,25 @@
+import Link from "next/link";
 import Links from "./links";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareArrowUpRight } from "@fortawesome/free-solid-svg-icons/faSquareArrowUpRight";
 
 const Hero = () => (
   <div className="grid px-14 py-48 gap-12">
-    <div className="w-1/2">
-      <p className="text-3xl text-[#FB923C]">Software Developer</p>
-      <p className="text-8xl text-white capitalize">Full stack web development</p>
-      <p className="text-8xl capitalize text-transparent font-bold bg-gradient-to-br from-transparent via-red-400 to-yellow-400 bg-clip-text">optimized</p>
+    <div className="w-1/2 grid gap-6">
+      <div>
+        <p className="text-3xl text-[#FB923C]">Software Developer</p>
+        <p className="text-8xl text-white capitalize">Full stack web development</p>
+        <p className="text-8xl capitalize text-transparent font-bold bg-gradient-to-br from-transparent via-teal-400 to-yellow-400 bg-clip-text">optimized</p>
+      </div>
+      <Link href="/cases" target="_blank" className="w-max">
+        <div className="flex flex-row gap-0 w-max">
+          <span className="rounded-full px-12 py-4 w-max gradient">Cases</span>
+          <div className="w-6 h-8 m-[-15px] mt-3 gradient"></div>
+          <span className="rounded-full px-4 py-4 w-max gradient">
+            <FontAwesomeIcon className="w-6 h-6" icon={faSquareArrowUpRight} />
+          </span>
+        </div>
+      </Link>
     </div>
     <Links />
   </div>
