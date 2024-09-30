b9fd3f82c2f7a5fac4aefaac5980a1ffa28b6607
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2024-09-30 11:27:30 +0200

    place hero to center

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "@/components/hero";
 
 const Home = () => (
-  <div className="w-full h-full overflow-y-auto">
+  <div className="w-full h-full overflow-y-auto grid place-content-center">
     <Hero />
   </div>
 );
