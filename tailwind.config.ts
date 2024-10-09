8ab325b4d45b5ad7cd06e4d26e0146666bfbb288
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2024-10-09 21:07:22 +0200

    tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './src/**/*.{html,svelte}',
+    './src/**/*.{html,svelte,js,ts}',
   ],
 }
 export default config
