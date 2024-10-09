6fbcf1afe973a317fbe868611aacb5312f3f168c
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2024-10-09 22:46:57 +0200

    head element

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,11 @@
 	import Stars from '../components/stars.svelte';
 </script>
 
+<svelte:head>
+	<title>Ivan Dimitrov</title>
+	<meta name="description" content="Freelance Software Developer" />
+</svelte:head>
+
 <main>
 	<Stars />
 	<slot />
