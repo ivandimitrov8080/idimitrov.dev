de3d968573f088d26c17bfa6e9eccd9002019946
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2024-10-12 19:35:10 +0200

    add TOC

--- src/routes/cases/[slug]/+page.ts
+++ src/routes/cases/[slug]/+page.ts
@@ -1,24 +1,10 @@
 import type { LoadProperties } from "@sveltejs/kit";
 import type { MetaTagsProps } from "svelte-meta-tags";
-// <MetaTags
-//   {title}
-//   titleTemplate="%s | idimitrov.dev"
-//   description={goal}
-//   canonical={$page.url.href}
-//   openGraph={{
-//     type: "website",
-//     locale: "en_GB",
-//     title: "idimitrov.dev",
-//     description: "Software Development",
-//     siteName: "idimitrov.dev"
-//   }}
-// />
 
 export async function load({ params }: LoadProperties<Record<string, any>>) {
   const post = await import(`../${params.slug}.svx`);
-  const { title, date, author, published, goal } = post.metadata;
+  const { title, date, author, published, goal, headers = [] } = post.metadata;
   const content = post.default;
-  const headers = [] as any;
   const pageMetaTags = Object.freeze({
     title: title,
     description: goal,
--- svelte.config.js
+++ svelte.config.js
@@ -4,12 +4,33 @@ import { mdsvex, escapeSvelte } from 'mdsvex';
 import remarkGfm from 'remark-gfm';
 import { sveltePreprocess } from 'svelte-preprocess';
 import { createHighlighter } from 'shiki';
+import { visit } from 'unist-util-visit'
+import { toString } from 'mdast-util-to-string'
+import { headingRank } from 'hast-util-heading-rank'
+import GithubSlugger from 'github-slugger'
 
 const theme = "github-dark"
 const highlighter = await createHighlighter({
   themes: [theme],
   langs: ['javascript', 'typescript', 'tsx', "jsx", "bash", "console", "html"]
 });
+
+const slugs = new GithubSlugger()
+function remarkSetHeaders() {
+  return function(tree, file) {
+    slugs.reset()
+    const headers = []
+    visit(tree, 'heading', function(node) {
+      const text = toString(node);
+      let h = {
+        id: slugs.slug(toString(node)),
+        text
+      };
+      headers.push(h)
+    })
+    file.data.fm.headers = headers;
+  }
+}
 
 const config = {
   kit: {
@@ -26,7 +47,7 @@ const config = {
     sveltePreprocess(),
     mdsvex({
       rehypePlugins: [rehypeSlug],
-      remarkPlugins: [remarkGfm],
+      remarkPlugins: [remarkGfm, remarkSetHeaders],
       highlight: {
         highlighter: async (code, lang = 'text') => {
           const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme }));
