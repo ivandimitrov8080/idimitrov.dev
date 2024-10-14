c012741a69f1996243345de9c481e365b1e24a2f
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2024-10-14 21:32:17 +0200

    add api

---
+++ api/src/main.rs
@@ -0,0 +1,36 @@
+use lettre::{Message, SmtpTransport, Transport};
+use rocket::{form::Form, response::Redirect};
+
+#[macro_use]
+extern crate rocket;
+
+#[derive(FromForm)]
+struct ContactForm<'r> {
+    name: &'r str,
+    email: &'r str,
+    message: &'r str,
+}
+
+#[post("/api/contact", data = "<contact_form>")]
+fn contact(contact_form: Form<ContactForm<'_>>) -> Redirect {
+    let name = contact_form.name;
+    let email = contact_form.email;
+    let message = contact_form.message;
+    let email = Message::builder()
+        .from(format!("{name} <{email}>").parse().unwrap())
+        .to("Ivan Kirilov Dimitrov <ivan@idimitrov.dev>"
+            .parse()
+            .unwrap())
+        .subject("Website contact form!")
+        .body(String::from(message))
+        .unwrap();
+    let sender = SmtpTransport::unencrypted_localhost();
+    let result = sender.send(&email);
+    println!("{:?}", result);
+    Redirect::to("/contact")
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/api", routes![contact])
+}
--- default.nix
+++ default.nix
@@ -15,6 +15,12 @@
           coreutils-full
           nodejs
           nodePackages_latest.prettier
+          rustc
+          rust-analyzer
+          rustfmt
+          cargo
+          pkg-config
+          openssl
           (nvim.extend {
             plugins = {
               lsp.servers = {
@@ -24,22 +30,42 @@
                 jsonls.enable = true;
                 tailwindcss.enable = true;
                 cssls.enable = true;
+                rust_analyzer = {
+                  enable = true;
+                  installCargo = false;
+                  installRustc = false;
+                };
+              };
+              rust-tools = {
+                enable = true;
               };
             };
           })
         ];
       };
-      packages.default = pkgs.buildNpmPackage {
-        pname = "idimitrov.dev";
-        version = "0.1.1";
-        src = ./.;
-        npmDepsHash = "sha256-5dBbBF16aUTEda3aGVpeLE/LF75b/Pay9AF12Ip1cRo=";
-        npmFlags = [ "--legacy-peer-deps" ];
-        postInstall = ''
-          rm -rf $out/*
-          rm -rf $out/.*
-          cp -r ./build/* $out/
-        '';
+      packages = {
+        default = pkgs.buildNpmPackage {
+          pname = "idimitrov.dev";
+          version = "0.1.1";
+          src = ./.;
+          npmDepsHash = "sha256-5dBbBF16aUTEda3aGVpeLE/LF75b/Pay9AF12Ip1cRo=";
+          npmFlags = [ "--legacy-peer-deps" ];
+          postInstall = ''
+            rm -rf $out/*
+            rm -rf $out/.*
+            cp -r ./build/* $out/
+          '';
+        };
+        api = pkgs.rustPlatform.buildRustPackage {
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = [ pkgs.openssl ];
+          pname = "api";
+          version = "0.0.1";
+          src = ./api;
+          cargoLock = {
+            lockFile = ./api/Cargo.lock;
+          };
+        };
       };
     };
   };
