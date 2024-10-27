7dcfa85900ea75645c141f016a0deb16343ac06a
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2024-10-27 23:12:49 +0100

    error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<div class="grid w-full h-full place-content-center">
+  <h1 class="text-9xl">{$page.status} {$page.error?.message}</h1>
+</div>
