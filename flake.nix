8935eeb7a09f6e1db2ebcf413766f150042974ad
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-02-09 21:46:42 +0100

    fix nixpkgs missing

--- flake.nix
+++ flake.nix
@@ -1,4 +1,8 @@
 {
-  inputs.configuration.url = "github:ivandimitrov8080/configuration.nix";
-  outputs = inputs: inputs.configuration.inputs.parts.lib.mkFlake { inherit inputs; } { imports = [ ./. ]; };
+  inputs = {
+    configuration.url = "github:ivandimitrov8080/configuration.nix";
+    nixpkgs.follows = "configuration/nixpkgs-unstable";
+  };
+  outputs =
+    inputs: inputs.configuration.inputs.parts.lib.mkFlake { inherit inputs; } { imports = [ ./. ]; };
 }
