21ab7ec5c7b62a008877e41760780b66b3d7c036
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-04-18 19:54:22 +0200

    add rocket config

--- default.nix
+++ default.nix
@@ -64,7 +64,10 @@
             nativeBuildInputs = [ pkgs.pkg-config ];
             buildInputs = [ pkgs.openssl ];
             pname = "api";
-            version = "0.0.1";
+            version = "0.0.2";
+            env = {
+              ROCKET_ENV = "prod";
+            };
             src = ./api;
             cargoLock = {
               lockFile = ./api/Cargo.lock;
