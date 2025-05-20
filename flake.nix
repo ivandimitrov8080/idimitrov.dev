c42f932d00499b89936b96a37f1a9103d090dee4
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-05-20 19:12:54 +0200

    follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     configuration.url = "github:ivandimitrov8080/configuration.nix";
-    nixpkgs.follows = "configuration/nixpkgs-unstable";
+    nixpkgs.follows = "configuration/nixpkgs";
   };
   outputs =
     inputs: inputs.configuration.inputs.parts.lib.mkFlake { inherit inputs; } { imports = [ ./. ]; };
