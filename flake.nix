4b645e0a1406db13315eea85a36b1d6dde17d100
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-11-02 12:21:17 +0100

    some tests

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
     let
       system = "x86_64-linux";
       pkgs = import inputs.nixpkgs { inherit system; };
+      lib = pkgs.lib;
       packages = {
         default = pkgs.buildNpmPackage {
           pname = "idimitrov.dev";
@@ -43,6 +44,34 @@
             lockFile = ./api/Cargo.lock;
           };
         };
+        testInteractive = test.driverInteractive;
+      };
+      extensions = [
+        "html"
+        "txt"
+        "png"
+        "jpg"
+        "jpeg"
+      ];
+      serveStatic = exts: ''
+        try_files ${lib.strings.concatStringsSep " " (builtins.map (x: "$uri.${x}") exts)} $uri $uri/ =404;
+      '';
+      webshiteConfig = {
+        enableACME = true;
+        forceSSL = true;
+        locations = {
+          "/" = {
+            root = "${packages.default}";
+            extraConfig = serveStatic extensions;
+          };
+          "/api" = {
+            proxyPass = "http://127.0.0.1:8000";
+          };
+        };
+        extraConfig = ''
+          add_header 'Referrer-Policy' 'origin-when-cross-origin';
+          add_header X-Content-Type-Options nosniff;
+        '';
       };
       nixosModules = {
         default =
@@ -56,40 +85,10 @@
               enable = mkEnableOption "enable webshite config";
             };
             config = mkIf cfg.enable {
-              services.nginx.virtualHosts =
-                let
-                  extensions = [
-                    "html"
-                    "txt"
-                    "png"
-                    "jpg"
-                    "jpeg"
-                  ];
-                  serveStatic = exts: ''
-                    try_files ${lib.strings.concatStringsSep " " (builtins.map (x: "$uri.${x}") exts)} $uri $uri/ =404;
-                  '';
-                  webshiteConfig = {
-                    enableACME = true;
-                    forceSSL = true;
-                    locations = {
-                      "/" = {
-                        root = "${packages.default}";
-                        extraConfig = serveStatic extensions;
-                      };
-                      "/api" = {
-                        proxyPass = "http://127.0.0.1:8000";
-                      };
-                    };
-                    extraConfig = ''
-                      add_header 'Referrer-Policy' 'origin-when-cross-origin';
-                      add_header X-Content-Type-Options nosniff;
-                    '';
-                  };
-                in
-                {
-                  "idimitrov.dev" = webshiteConfig;
-                  "www.idimitrov.dev" = webshiteConfig;
-                };
+              services.nginx.virtualHosts = {
+                "idimitrov.dev" = webshiteConfig;
+                "www.idimitrov.dev" = webshiteConfig;
+              };
               systemd.services.webshiteApi = {
                 enable = true;
                 serviceConfig = {
@@ -101,9 +100,60 @@
             };
           };
       };
+      client = {
+        default =
+          { pkgs, ... }:
+          {
+            environment.systemPackages = with pkgs; [
+              curl
+              gnugrep
+            ];
+            systemd.network.enable = true;
+            networking.useNetworkd = true;
+          };
+      };
+      server = {
+        default =
+          { pkgs, config, ... }:
+          {
+            imports = [ nixosModules.default ];
+            networking.firewall.allowedTCPPorts = [
+              80
+              443
+            ];
+            networking.firewall.allowedUDPPorts = [
+              80
+              443
+            ];
+            systemd.network.enable = true;
+            networking.useNetworkd = true;
+            webshite.enable = true;
+            services.nginx.enable = true;
+            security.acme.defaults.server = "https://acme-staging-v02.api.letsencrypt.org/directory";
+            security.acme.defaults.email = "test@example.com";
+            security.acme.acceptTerms = true;
+          };
+      };
+      test = pkgs.testers.runNixOSTest {
+        name = "test";
+        nodes = {
+          server = server.default;
+          client1 = client.default;
+        };
+        testScript =
+          #py
+          ''
+            start_all()
+            client1.wait_for_unit("default.target")
+            server.wait_for_unit("nginx.service")
+            client1.succeed("curl http://server | grep -o '301'")
+            client1.succeed("curl -k https://server | grep -o 'Home | idimitrov.dev'")
+          '';
+      };
     in
     {
       nixosModules = nixosModules;
       packages.${system} = packages;
+      checks.${system}.default = test;
     };
 }
