7aa0d81e77a826858694d1ac993bd8f2a890ba69
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2026-01-09 16:04:05 +0100

    elm canvas experiments

--- elm-srcs.nix
+++ elm-srcs.nix
@@ -1,5 +1,10 @@
 {
 
+  "avh4/elm-color" = {
+    sha256 = "0n16wnvp87x9az3m5qjrl6smsg7051m719xn5d244painx8xmpzq";
+    version = "1.0.0";
+  };
+
   "elm/browser" = {
     sha256 = "0nagb9ajacxbbg985r4k9h0jadqpp0gp84nm94kcgbr5sf8i9x13";
     version = "1.0.2";
@@ -15,19 +20,39 @@
     version = "1.0.1";
   };
 
-  "elm/random" = {
-    sha256 = "138n2455wdjwa657w6sjq18wx2r0k60ibpc4frhbqr50sncxrfdl";
-    version = "1.0.0";
-  };
-
   "elm/json" = {
     sha256 = "0w1n61m2b9zbwp1yx8zhvir7gqy66mkm22rpq8hmnvgkcwnhxalb";
     version = "1.1.4";
   };
 
+  "elm/random" = {
+    sha256 = "138n2455wdjwa657w6sjq18wx2r0k60ibpc4frhbqr50sncxrfdl";
+    version = "1.0.0";
+  };
+
   "elm/time" = {
     sha256 = "0vch7i86vn0x8b850w1p69vplll1bnbkp8s383z7pinyg94cm2z1";
     version = "1.0.0";
+  };
+
+  "ianmackenzie/elm-geometry" = {
+    sha256 = "1z7vpdrd2pkckcnnal5pa2psgc8w3mwhxjv8wjzfgy0n84hi2vf7";
+    version = "4.0.0";
+  };
+
+  "ianmackenzie/elm-units" = {
+    sha256 = "053nibhpkf906c7g14z48pvinzzfhnrnk5hkj4mjvjnv3r457h97";
+    version = "2.10.0";
+  };
+
+  "joakin/elm-canvas" = {
+    sha256 = "1znw3c7amj21dik7b16lyjkn83z5fh82cma0ii7sax24k0f6lsl2";
+    version = "5.0.0";
+  };
+
+  "joakin/elm-grid" = {
+    sha256 = "1g60x44hhbq5i6naggna070y82fy5xx1aii1a6y82h2n47n3xxp7";
+    version = "1.0.1";
   };
 
   "elm/url" = {
@@ -38,5 +63,30 @@
   "elm/virtual-dom" = {
     sha256 = "0zc3dfzqsma8lrkbm6nkjqbsfcv6h5nja1rh7ybkinw1jk0kfdv6";
     version = "1.0.5";
+  };
+
+  "ianmackenzie/elm-1d-parameter" = {
+    sha256 = "08r1q80k7w2kyasqk54k1xzd5d1607wj4ahx66jdh7gkrni9m8n5";
+    version = "1.0.1";
+  };
+
+  "ianmackenzie/elm-float-extra" = {
+    sha256 = "0l499d14iabyigdysvx3xa572izmqqki7b1ym2gc7kv4sprrq194";
+    version = "1.1.0";
+  };
+
+  "ianmackenzie/elm-interval" = {
+    sha256 = "086x40s9gnl2r7700ff75w0p3wfrxh144ks24ni5cwhirxwwf0pq";
+    version = "3.1.0";
+  };
+
+  "ianmackenzie/elm-triangular-mesh" = {
+    sha256 = "00n08cc3qj610g2f7f77qpl6b6pwr456czf3prkz6iddmi52h3gp";
+    version = "1.1.0";
+  };
+
+  "ianmackenzie/elm-units-interval" = {
+    sha256 = "0xwb5vj8jcnn88cnwniibgncfvyaxnmwwkrbk1n7ji3aj6w8f8dj";
+    version = "3.2.0";
   };
 }
--- src/Main.elm
+++ src/Main.elm
@@ -1,81 +1,131 @@
-module Main exposing (..)
-
--- Press buttons to increment and decrement a counter.
---
--- Read how it works:
---   https://guide.elm-lang.org/architecture/buttons.html
---
+module Main exposing (main)
 
 import Browser
-import Html exposing (Html, br, button, div, text)
-import Html.Events exposing (onClick)
-import Random
+import Browser.Events exposing (onAnimationFrame)
+import Canvas exposing (..)
+import Canvas.Settings exposing (..)
+import Color exposing (Color)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Time exposing (Posix)
 
 
+type alias Point =
+    { x : Float
+    , y : Float
+    , size : Float
+    , deviation : Float
+    , speedMod : Float
+    }
 
--- MAIN
+
+type alias Model =
+    List Point
+
+
+type Msg
+    = AnimationFrame Posix
 
 
 main : Program () Model Msg
 main =
-    Browser.element
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = always Sub.none
-        }
+    Browser.element { init = init, update = update, subscriptions = subscriptions, view = view }
 
 
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    onAnimationFrame AnimationFrame
 
--- MODEL
+
+h : Float
+h =
+    500
 
 
-type alias Model =
-    { value : Int, inc : Int }
+w : Float
+w =
+    500
+
+
+padding : Float
+padding =
+    w / 6
+
+
+cellW : Float
+cellW =
+    w - (padding * 2)
+
+
+cellH : Float
+cellH =
+    h - (padding * 2)
+
+
+particleColor : Color
+particleColor =
+    Color.rgba 0 0 0 0.3
+
+
+numParticles : Int
+numParticles =
+    1000
 
 
 init : () -> ( Model, Cmd Msg )
-init _ =
-    ( Model 0 0, Cmd.none )
-
-
-
--- UPDATE
-
-
-type Msg
-    = Increment
-    | Decrement
-    | GenerateRandom
-    | NewRandom Int
+init () =
+    ( List.range 0 numParticles
+        |> List.map
+            (\i ->
+                { x = w / 2
+                , y = h / 2
+                , size = toFloat (modBy 2 i + 1)
+                , speedMod = toFloat (modBy 345 (i * 4236))
+                , deviation = toFloat (modBy 4435 (i * 2346))
+                }
+            )
+    , Cmd.none
+    )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Increment ->
-            ( { model | value = model.value + model.inc }, Cmd.none )
+        AnimationFrame time ->
+            let
+                timef =
+                    time |> Time.posixToMillis |> toFloat
 
-        Decrement ->
-            ( { model | value = model.value - model.inc }, Cmd.none )
+                normalize x =
+                    (x + 1) / 2
 
-        NewRandom n ->
-            ( { model | inc = n }, Cmd.none )
-
-        GenerateRandom ->
-            ( model, Random.generate NewRandom (Random.int 1 100) )
-
-
-
--- VIEW
+                updatePoint point =
+                    { point
+                        | x =
+                            normalize (sin ((timef / (300 + point.speedMod)) + point.deviation))
+                                * cellW
+                                + padding
+                        , y =
+                            normalize (cos ((timef / (500 - point.speedMod)) + point.deviation + 4543))
+                                * cellH
+                                + padding
+                    }
+            in
+            ( List.map updatePoint model
+            , Cmd.none
+            )
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (String.fromInt model.value) ]
-        , button [ onClick Increment ] [ text "+" ]
-        , br [] []
-        , button [ onClick GenerateRandom ] [ text "+" ]
+    Canvas.toHtml
+        ( round w, round h )
+        []
+        [ shapes [ fill Color.white ] [ rect ( 0, 0 ) w h ]
+        , shapes [ fill particleColor ] (List.map drawPoint model)
         ]
+
+
+drawPoint : Point -> Shape
+drawPoint { x, y, size } =
+    circle ( x, y ) size
