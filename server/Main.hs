5b9c30c07e15bce196a2b91546cbaa3c1af7468f
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2026-01-15 00:09:58 +0100

    cleanup

--- server/Main.hs
+++ server/Main.hs
@@ -101,36 +101,6 @@ getItemById = \case
 exampleItem :: Item
 exampleItem = Item 0 "example item" "a"
 
-exampleItem2 :: Item
-exampleItem2 = Item 1 "example item 2" "b"
-
-exampleItem3 :: Item
-exampleItem3 = Item 2 "example item 3" "c"
-
-exampleItem4 :: Item
-exampleItem4 = Item 3 "example item 4" "s"
-
-sumAndDivModSession :: Int64 -> Int64 -> Int64 -> Session (Int64, Int64)
-sumAndDivModSession a b c = do
-  -- Get the sum of a and b
-  sumOfAAndB <- Session.statement (a, b) sumStatement
-  -- Divide the sum by c and get the modulo as well
-  Session.statement (sumOfAAndB, c) divModStatement
-
-sumStatement :: Statement (Int64, Int64) Int64
-sumStatement =
-  [TH.singletonStatement|
-    select ($1 :: int8 + $2 :: int8) :: int8
-  |]
-
-divModStatement :: Statement (Int64, Int64) (Int64, Int64)
-divModStatement =
-  [TH.singletonStatement|
-    select
-      (($1 :: int8) / ($2 :: int8)) :: int8,
-      (($1 :: int8) % ($2 :: int8)) :: int8
-  |]
-
 --------------------------------------------------------------------------------
 -- Serve
 --------------------------------------------------------------------------------
