999e704cb5ed5fdd737d4f1e9b09aea623e08d81
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2026-01-17 17:21:55 +0100

    allow cors post

--- server/App.hs
+++ server/App.hs
@@ -6,10 +6,19 @@ import Api
 import DB (Pool)
 import Handlers (AppM, runAppM, server)
 import Network.Wai (Application)
-import Network.Wai.Middleware.Cors (cors, simpleCorsResourcePolicy)
+import Network.Wai.Middleware.Cors (CorsResourcePolicy (corsMethods, corsRequestHeaders), cors, simpleCorsResourcePolicy)
 import Servant (hoistServer, serve)
 
 mkApp :: Pool -> IO Application
 mkApp pool = do
   let apiApp = serve api (hoistServer api (runAppM pool) server)
-  pure $ cors (const $ Just simpleCorsResourcePolicy) apiApp
+  pure $
+    cors
+      ( const $
+          Just
+            simpleCorsResourcePolicy
+              { corsRequestHeaders = ["Content-Type", "Authorization"],
+                corsMethods = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
+              }
+      )
+      apiApp
