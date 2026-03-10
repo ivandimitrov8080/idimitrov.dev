6df65b3f09aa96320f6e03c6cf2efcdb18d2bfe6
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2026-03-10 13:40:27 +0100

    insert hashed passwords

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,1 +1,7 @@
-INSERT INTO account(name, password) VALUES('user1', 'password1'), ('user2', 'password2'), ('user3', 'password3');
+INSERT INTO account(name, password) VALUES
+  -- password1
+  ('user1', '$argon2id$v=19$m=65536,t=2,p=1$zjMGhAEfI+qkznRRLI8yCw$wFOOydEpHpidS/y5e/UQF93vgOzdh+94Y/s9tunpq64'),
+  -- password2
+  ('user2', '$argon2id$v=19$m=65536,t=2,p=1$ITgHPtt6SKoCmazycRaQlQ$Kd59NJrOmAILIG8sv8tj1VAM+dUhd7SzQBedF24Bh8k'),
+  -- password3
+  ('user3', '$argon2id$v=19$m=65536,t=2,p=1$fdL8IxeoojZTaJP86UxswQ$6Bauca0QV6HcvVCDNPrHSwyeozp3jJ7DIh2fdeUV6Yo');
