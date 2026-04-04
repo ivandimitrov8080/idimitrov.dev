a8f7d451bef2cacf40209620619656d921a56fc7
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2026-04-04 14:06:50 +0200

    cleanup elm deps

--- elm-srcs.nix
+++ elm-srcs.nix
@@ -10,16 +10,6 @@
     version = "2.0.2";
   };
 
-  "elm-explorations/linear-algebra" = {
-    sha256 = "19zf3dgrx5bvhybjwq3bp171wzsb4bfk5rrfjv234b2f8q3yi59g";
-    version = "1.0.3";
-  };
-
-  "elm-explorations/webgl" = {
-    sha256 = "0ma7laa3ax8zd41s838px1y5s1dsmlqzp4j321lwngxjzvxkm3zy";
-    version = "1.1.3";
-  };
-
   "elm/browser" = {
     sha256 = "0nagb9ajacxbbg985r4k9h0jadqpp0gp84nm94kcgbr5sf8i9x13";
     version = "1.0.2";
@@ -60,26 +50,6 @@
     version = "1.0.0";
   };
 
-  "ianmackenzie/elm-geometry" = {
-    sha256 = "1z7vpdrd2pkckcnnal5pa2psgc8w3mwhxjv8wjzfgy0n84hi2vf7";
-    version = "4.0.0";
-  };
-
-  "ianmackenzie/elm-units" = {
-    sha256 = "053nibhpkf906c7g14z48pvinzzfhnrnk5hkj4mjvjnv3r457h97";
-    version = "2.10.0";
-  };
-
-  "joakin/elm-canvas" = {
-    sha256 = "1znw3c7amj21dik7b16lyjkn83z5fh82cma0ii7sax24k0f6lsl2";
-    version = "5.0.0";
-  };
-
-  "joakin/elm-grid" = {
-    sha256 = "1g60x44hhbq5i6naggna070y82fy5xx1aii1a6y82h2n47n3xxp7";
-    version = "1.0.1";
-  };
-
   "elm/bytes" = {
     sha256 = "02ywbf52akvxclpxwj9n04jydajcbsbcbsnjs53yjc5lwck3abwj";
     version = "1.0.8";
@@ -93,30 +63,5 @@
   "elm/virtual-dom" = {
     sha256 = "0zc3dfzqsma8lrkbm6nkjqbsfcv6h5nja1rh7ybkinw1jk0kfdv6";
     version = "1.0.5";
-  };
-
-  "ianmackenzie/elm-1d-parameter" = {
-    sha256 = "08r1q80k7w2kyasqk54k1xzd5d1607wj4ahx66jdh7gkrni9m8n5";
-    version = "1.0.1";
-  };
-
-  "ianmackenzie/elm-float-extra" = {
-    sha256 = "0l499d14iabyigdysvx3xa572izmqqki7b1ym2gc7kv4sprrq194";
-    version = "1.1.0";
-  };
-
-  "ianmackenzie/elm-interval" = {
-    sha256 = "086x40s9gnl2r7700ff75w0p3wfrxh144ks24ni5cwhirxwwf0pq";
-    version = "3.1.0";
-  };
-
-  "ianmackenzie/elm-triangular-mesh" = {
-    sha256 = "00n08cc3qj610g2f7f77qpl6b6pwr456czf3prkz6iddmi52h3gp";
-    version = "1.1.0";
-  };
-
-  "ianmackenzie/elm-units-interval" = {
-    sha256 = "0xwb5vj8jcnn88cnwniibgncfvyaxnmwwkrbk1n7ji3aj6w8f8dj";
-    version = "3.2.0";
   };
 }
--- src/Main.elm
+++ src/Main.elm
@@ -1,32 +1,17 @@
 module Main exposing (main)
 
 import Browser
-import Browser.Events exposing (onAnimationFrame, onClick)
 import Canvas exposing (..)
 import Canvas.Settings exposing (..)
-import Color exposing (Color)
-import Cube
 import Generated.Api exposing (Account, LoginResponse, Profile, getProfile, postLogin, postRegister)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 import Http exposing (Error(..))
-import Time exposing (Posix)
-
-
-type alias Point =
-    { x : Float
-    , y : Float
-    , size : Float
-    , deviation : Float
-    , speedMod : Float
-    }
 
 
 type alias Model =
-    { pts : List Point
-    , cubeTheta : Float
-    , errors : List Error
+    { errors : List Error
     , account : Account
     , token : Maybe String
     , profile : Maybe Profile
@@ -34,8 +19,7 @@ type alias Model =
 
 
 type Msg
-    = AnimationFrame Posix
-    | Register Account
+    = Register Account
     | Login Account
     | RegisterSuccess (Result Http.Error LoginResponse)
     | LoginSuccess (Result Http.Error LoginResponse)
@@ -51,59 +35,12 @@ main =
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-    onAnimationFrame AnimationFrame
-
-
-h : Float
-h =
-    500
-
-
-w : Float
-w =
-    500
-
-
-padding : Float
-padding =
-    w / 6
-
-
-cellW : Float
-cellW =
-    w - (padding * 2)
-
-
-cellH : Float
-cellH =
-    h - (padding * 2)
-
-
-particleColor : Color
-particleColor =
-    Color.rgba 0 0 0 0.3
-
-
-numParticles : Int
-numParticles =
-    1000
+    Sub.none
 
 
 init : () -> ( Model, Cmd Msg )
 init () =
-    ( { pts =
-            List.range 0 numParticles
-                |> List.map
-                    (\i ->
-                        { x = w / 2
-                        , y = h / 2
-                        , size = toFloat (modBy 2 i + 1)
-                        , speedMod = toFloat (modBy 345 (i * 4236))
-                        , deviation = toFloat (modBy 4435 (i * 2346))
-                        }
-                    )
-      , cubeTheta = 0
-      , errors = []
+    ( { errors = []
       , account = Account Nothing "anon" "" Nothing
       , token = Nothing
       , profile = Nothing
@@ -115,30 +52,6 @@ init () =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        AnimationFrame time ->
-            let
-                timef =
-                    time |> Time.posixToMillis |> toFloat
-
-                normalize x =
-                    (x + 1) / 2
-
-                updatePoint point =
-                    { point
-                        | x =
-                            normalize (sin ((timef / (300 + point.speedMod)) + point.deviation))
-                                * cellW
-                                + padding
-                        , y =
-                            normalize (cos ((timef / (500 - point.speedMod)) + point.deviation + 4543))
-                                * cellH
-                                + padding
-                    }
-            in
-            ( { model | pts = List.map updatePoint model.pts, cubeTheta = model.cubeTheta + 0.005 }
-            , Cmd.none
-            )
-
         Register account ->
             ( model, postRegister account RegisterSuccess )
 
@@ -218,14 +131,7 @@ errorToString e =
 view : Model -> Html Msg
 view model =
     div []
-        [ Canvas.toHtml
-            ( round w, round h )
-            []
-            [ shapes [ fill Color.white ] [ rect ( 0, 0 ) w h ]
-            , shapes [ fill particleColor ] (List.map drawPoint model.pts)
-            ]
-        , Cube.view model.cubeTheta
-        , div [] [ Html.text model.account.accountName ]
+        [ div [] [ Html.text model.account.accountName ]
         , div []
             [ div []
                 [ label [ for "username" ] [ Html.text "Username" ]
@@ -265,8 +171,3 @@ view model =
             , div [] (List.map (\e -> span [] [ Html.text (errorToString e) ]) model.errors)
             ]
         ]
-
-
-drawPoint : Point -> Shape
-drawPoint { x, y, size } =
-    circle ( x, y ) size
