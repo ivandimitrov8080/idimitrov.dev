d725c8f26a046d9c9d124d5f0cca496fbe7542b8
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2026-04-30 23:18:09 +0200

    update

--- flake.nix
+++ flake.nix
@@ -283,18 +283,6 @@
                     exec = "hurl test/server/login.hurl";
                   };
                 };
-                git-hooks.hooks = {
-                  nixfmt.enable = true;
-                  prettier.enable = true;
-                  prettier.package = pkgs.prettier;
-                  elm-format.enable = true;
-                  deadnix.enable = true;
-                  statix.enable = true;
-                  ormolu.enable = true;
-                  ormolu.settings.defaultExtensions = [
-                    "ImportQualifiedPost"
-                  ];
-                };
               }
             ];
           };
